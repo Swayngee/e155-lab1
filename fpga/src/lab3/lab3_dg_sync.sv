dfffd41cd5fb69964fbb3b95abbb2279af0b7292
Swayngee <drgonzales@g.hmc.edu>
2025-09-20 05:54:43 -0700

    Update lab3_dg_sync.sv

--- fpga/src/lab3/lab3_dg_sync.sv
+++ fpga/src/lab3/lab3_dg_sync.sv
@@ -1,5 +1,9 @@
+// Drake Gonzales
+// drgonzales@g.hmc.edu
+// This Module holds synchronzer for my fsm
+// 9/20/25
 module lab3_dg_sync(input logic int_osc,
-					input logic reset,
+					input logic reset,e
                     input logic [3:0] rows,
                     output logic [3:0] sync);
 logic [3:0] n1;
