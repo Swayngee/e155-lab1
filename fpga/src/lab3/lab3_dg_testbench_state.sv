c8192a6260372fe4907ef57564bac5786be41909
Swayngee <drgonzales@g.hmc.edu>
2025-09-22 21:02:34 -0700

    Add files via upload

--- fpga/src/lab3/lab3_dg_testbench_state.sv
+++ fpga/src/lab3/lab3_dg_testbench_state.sv
@@ -15,7 +15,7 @@ logic [3:0] exp;
 logic [31:0] vectornum, errors;
 integer a;
 
-lab3_dg_state u_state(int_osc, reset, rows, sync, cols, keypress, rowpress, alarm);
+lab3_dg_state u_state(int_osc, reset, sync, cols, keypress, alarm);
 
 always
 begin
@@ -29,48 +29,20 @@ reset=1; #22;
 reset=0;
 errors = 0;
 rows = 4'b1111;
-sync = 4'b0001; 
+sync = 4'b1111; 
 
-
-// state idle testing
-	if (u_state.state == u_state.idle) begin
-		assert((u_state.nextstate == u_state.waiter) && (rows != 4'b1111))
-		else $error("idle state failed");
-	end
-// state waiter testing
-	if (u_state.state == u_state.waiter) begin
-		assert((u_state.keypress == {cols, u_state.srows}) && (u_state.nextstate == u_state.check))
-		else $error("sync state failed");
-	end
-// state check test
-	if (u_state.state == u_state.check) begin
-		assert((u_state.keypress != u_state.prekey) && (u_state.nextstate == u_state.drive)) 
-		else $error("check state failed");
-	end
-// state drive test
-	 if (u_state.state == u_state.drive) begin
-		assert(u_state.nextstate == u_state.last)
-			else $error("drive state failed");
-		end
-// state last test
-	if (u_state.state == u_state.last) begin
-		assert((u_state.keypress == u_state.prekey) && (u_state.nextstate == u_state.idle)) 
-		else $error("check state failed");
-	end
 
 for (a = 0; a < 4; a++) begin
     exp  = ~(1 << a);  
-    rows = exp;       
+    sync = exp;       
     repeat (20) @(posedge int_osc);
 
-    if (rows !== keypress[3:0]) begin
+    if (sync !== keypress[3:0]) begin
         $display("expected row %b, got %b", rows, keypress[3:0]);
          errors= errors+1;
      end else begin
         $display("tests completed with %d errors", errors);
     end
-        rows = 4'b1111;
-		sync = 4'b1111;		
          repeat (4) @(posedge int_osc);
         end
     $stop;
